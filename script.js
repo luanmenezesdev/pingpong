1e2adfd5e5e7f338a0d98bad5eb05b36974da653
Luan Menezes <luan.vini1@gmail.com>
2023-12-07 19:08:04 -0300

    feat: create player paddle

--- script.js
+++ script.js
@@ -11,10 +11,29 @@ const ball = {
         y: canvas.height / 2,
     },
     velocity: {
-        x: -2,
-        y: -2,
+        x: 2,
+        y: 2,
     },
     radius: 10,
+}
+
+const paddlePlayer = {
+    position: {
+        x: 10,
+        y: 0,
+    },
+    sizes: {
+        width: 8,
+        height: canvas.height / 5,
+    }
+}
+
+function renderPaddle(paddle) {
+    const { position, sizes } = paddle;
+
+    context.beginPath();
+    context.rect(position.x, position.y, sizes.width, sizes.height);
+    context.fill();
 }
 
 function renderBall(ball) {
@@ -28,22 +47,60 @@ function renderBall(ball) {
 function changeBallPosition(ball) {
     const { position, velocity, radius } = ball;
 
-    if (position.y + radius >= canvas.height) position.y = canvas.height - radius;
-    if (position.x + radius >= canvas.width) position.x = canvas.width - radius;
-    if (position.y - radius <= 0) position.y = radius;
-    if (position.x - radius <= 0) position.x = radius;
+    if (position.y + radius >= canvas.height) {
+        changeDirection(velocity, 'y');
+    };
+
+    if (position.x + radius >= canvas.width) {
+        changeDirection(velocity, 'x');
+    };
+    
+    if (position.y - radius <= 0) {
+        changeDirection(velocity, 'y');
+    };
+
+    if (position.x - radius <= 0) {
+        changeDirection(velocity, 'x');
+    };
 
     position.x += velocity.x;
     position.y += velocity.y;
 }
 
+function changePaddlePosition(paddle, clientY) {
+    const rect = canvas.getBoundingClientRect();
+    const paddleHeight = paddle.sizes.height;
+
+    const outsideBottom = clientY + (paddleHeight / 2) >= rect.bottom;
+    const outsideTop = clientY - (paddleHeight / 2) <= rect.top;
+
+    if (outsideBottom || outsideTop) return;
+
+    paddle.position.y = clientY - rect.top - paddleHeight / 2;
+}
+
+canvas.addEventListener('mousemove', (event) => {
+    const { clientX, clientY } = event;
+    
+    changePaddlePosition(paddlePlayer, clientY)
+});
+
+function changeDirection(velocity, direction) {
+    velocity[direction] = -velocity[direction];
+}
+
+function gameLoop() {
+    renderBall(ball);
+    renderPaddle(paddlePlayer);
+    changeBallPosition(ball);
+}
+
 
 function update() {
     context.clearRect(0, 0, canvas.width, canvas.height);
-    renderBall(ball);
-    changeBallPosition(ball);
+    gameLoop();
 
-    requestAnimationFrame(update)
+    requestAnimationFrame(update);
 }
 
 update();
